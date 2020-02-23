11e2f851404ce064056a9e4ff12884f5c0a93c09
Kitiya <kate.kitiya@gmail.com>
2020-02-23 03:15:43 -0500

    Fix netlify's deployment

---
+++ src/components/a/a.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./a.styles.scss";
+
+const A = () => <div>xxx</div>;
+
+export default A;
--- src/components/a/collection-item.component.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./collection-item.styles.scss";
-
-const CollectionItem = () => <div>xxx</div>;
-
-export default CollectionItem;
