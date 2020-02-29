11be16e6736d29aab62d5a49a99db948c9045362
Kitiya <kate.kitiya@gmail.com>
2020-02-29 15:02:44 -0500

    Create 'itemCount' and 'cartItems'  memoizied selectors using 'reselect' library

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
 
@@ -18,7 +19,7 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,21 @@
+import { createSelector } from "reselect";
+
+// this is a "input selector".
+const selectCart = state => state.cart;
+
+// Using createSelector() from the 'reselect' library to create a memoir selector
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+// a memoir selector
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
