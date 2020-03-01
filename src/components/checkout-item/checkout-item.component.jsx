6e29996d089e089c99d6d8df2042c7c5bbe15dc8
Kitiya <kate.kitiya@gmail.com>
2020-03-01 04:30:47 -0500

    Add addItem and removeItem functionality to the CheckoutItem component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,22 +1,49 @@
 import React from "react";
-import "./checkout-item.styles.scss";
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from "../../redux/cart/cart.actions";
+
 import { setLocalCurrencyFormat } from "../../utils/formatter.utils";
+import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({
-  cartItem: { id, name, imageUrl, price, quantity }
-}) => (
-  <div className="checkout-item">
-    <div className="image-wrapper">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      ></div>
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-wrapper">
+        <div
+          className="background-image"
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        ></div>
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <i
+          className="arrow fa fa-minus-square-o"
+          onClick={() => removeItem(cartItem)}
+        ></i>
+        <span className="quantity-value">{quantity}</span>
+        <i
+          className="arrow fa fa-plus-square-o"
+          onClick={() => addItem(cartItem)}
+        ></i>
+      </span>
+      <span className="price">{setLocalCurrencyFormat(price)}</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{setLocalCurrencyFormat(price)}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
