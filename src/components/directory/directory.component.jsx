4d1ed88ae6b22cae06b8b02ef4781bae24e4f886
Kitiya <kate.kitiya@gmail.com>
2020-03-01 18:19:19 -0500

    Create directoryReducer with DIRECTORY_DATA as an initial state

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import "./directory.styles.scss";
-import MenuItem from "../menu-item/menu-item.component";
-import { DIRECTORY_DATA } from "./directory.data";
+import { connect } from "react-redux";
 
-const Directory = title => {
-  const sections = DIRECTORY_DATA;
+import { createStructuredSelector } from "reselect";
+import { selectDirectorySections } from "../../redux/directory/directory.selector";
+
+import MenuItem from "../menu-item/menu-item.component";
+
+import "./directory.styles.scss";
+
+const Directory = ({ sections }) => {
   return (
     <div className="directory-menu">
       {sections.map(({ id, title, imageUrl, size }) => (
@@ -14,4 +18,8 @@ const Directory = title => {
   );
 };
 
-export default Directory;
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,16 +6,18 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["cart"] // the reducer that we want to persist our data
+  whitelist: ["cart", "directory"] // the reducer that we want to persist our data
 };
 
 const rootReducer = combineReducers({
   user: userReducer, // handled by Firebase
-  cart: cartReducer // add it to the 'whitelist'
+  cart: cartReducer, // add it to the 'whitelist'
+  directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
